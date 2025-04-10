a60fc9b26504b225287fd9c3280a5e0929775c87
Ravi kumar Rai <raviengineer2001@gmail.com>
2025-04-10 14:56:01 +0530

    Initial Commit

---
+++ public/Home.jsx
@@ -0,0 +1,8 @@
+export function Home() {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+        <p className="text-lg text-gray-600">I am a Web Developer passionate about building great websites.</p>
+      </div>
+    );
+  }
